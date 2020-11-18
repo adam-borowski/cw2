4d46c128881ae0b116569d79eb790bcb9e930054
Adam <adam@hp.probook>
2020-11-18 23:14:00 +0100

    catch exceptions

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace cw2
 {
@@ -6,11 +7,34 @@ namespace cw2
     {
         static void Main(string[] args)
         {
-            var reader = new CsvReader("dane.csv");
-            var list = reader.Read();
-            var students = new StudentsList(list).Validate();
-            var xmlSaver = new XmlSaver<Student>(students);
-            xmlSaver.Save("data.xml");
+            if (args.Length == 0) {
+                throw new ArgumentNullException("Nie podano parametrów.");
+            }
+            
+            try {
+                var reader = new CsvReader(args[0]);
+                var list = reader.Read();
+                var students = new StudentsList(list).Validate();
+                var fileName = "result.xml";
+                var format = "xml";
+
+                if (args.Length >= 2) {
+                    fileName = args[1];
+                }
+
+                if (args.Length == 3) {
+                    format = args[2];
+                }
+
+                if (format == "xml") {
+                    var xmlSaver = new XmlSaver<Student>(students);
+                    xmlSaver.Save(fileName);
+                } else {
+                    throw new ArgumentOutOfRangeException("Nieznany format: " + format);
+                }
+            } catch (FileNotFoundException) {
+                throw new FileNotFoundException("Nie znaleziono pliku: " + args[0]);   
+            }
         }
     }
 }
